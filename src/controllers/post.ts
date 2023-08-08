92753f3903a27a9f9c01db3d26d401a0096497fe
paugarcia32 <paugarcia32@gmail.com>
2023-08-08 17:47:25 +0200

    create post?

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -5,6 +5,8 @@ import {
   createPostService,
 } from "../services/post";
 import { handleHttp } from "../utils/error.handle";
+import { verifyToken } from "../utils/jwt.handle";
+const fs = require("fs");
 
 const getPosts = async (req: Request, res: Response) => {
   try {
@@ -20,8 +22,51 @@ const getPosts = async (req: Request, res: Response) => {
     handleHttp(res, "Error al obtener publicaciones");
   }
 };
-const getAllPosts = async (req: Request, res: Response) => {};
+const getAllPosts = async (req: Request, res: Response) => {
+  try {
+    const response = await getAllPostsService();
+    res.json(response);
+  } catch (e) {
+    console.error(e);
+    handleHttp(res, "Error al obtener todas publicaciones");
+  }
+};
 
-const createPost = async (req: Request, res: Response) => {};
+const createPost = async (req: Request, res: Response) => {
+  try {
+    if (!req.file) {
+      throw new Error("No file received.");
+    }
+
+    const { originalname, path } = req.file;
+    if (!originalname || !path) {
+      throw new Error("Invalid file data received.");
+    }
+
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+
+    const { token } = req.cookies;
+    verifyToken(req, res, async () => {
+      const { title, summary, content, tag } = req.body;
+      const tagsArray = Array.isArray(tag) ? tag : [tag];
+
+      const postDoc = await createPostService({
+        title,
+        summary,
+        content,
+        cover: newPath,
+        authorId: req.user?.id || "", // Usar el ID del usuario del token
+        tags: tagsArray,
+      });
+
+      res.json(postDoc);
+    });
+  } catch (error) {
+    handleHttp(res, "Error al crear post");
+  }
+};
 
 export { getPosts, getAllPosts, createPost };
--- src/services/post.ts
+++ src/services/post.ts
@@ -4,6 +4,7 @@ import { ITag } from "../interfaces/tag.interface";
 import { IUser } from "../interfaces/user.interface";
 import { IPost } from "../interfaces/post.interface";
 import Tag from "../models/Tag";
+const fs = require("fs");
 
 const getPostService = async (currentPage: number, postsPerPage: number) => {
   try {
@@ -27,8 +28,54 @@ const getPostService = async (currentPage: number, postsPerPage: number) => {
   }
 };
 
-const getAllPostsService = async () => {};
+const getAllPostsService = async () => {
+  const posts = await Post.find()
+    .populate("author", ["username"])
+    .populate("tag", ["title"])
+    .sort({ createdAt: -1 });
+  return { posts };
+};
 
-const createPostService = async () => {};
+interface CreatePostParams {
+  title: string;
+  summary: string;
+  content: string;
+  cover: string;
+  authorId: string;
+  tags: string[];
+}
+
+const createPostService = async ({
+  title,
+  summary,
+  content,
+  cover,
+  authorId,
+  tags,
+}: CreatePostParams) => {
+  const tagsArray = tags.map((tag) => new Types.ObjectId(tag));
+
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    cover,
+    author: new Types.ObjectId(authorId),
+    tag: tagsArray,
+  });
+
+  const postId = postDoc._id;
+
+  for (const tagId of tagsArray) {
+    const tagToUpdate = await Tag.findById(tagId);
+    if (!tagToUpdate) {
+      throw new Error(`El tag con ID ${tagId} no existe.`);
+    }
+    tagToUpdate.posts.push(postId);
+    await tagToUpdate.save();
+  }
+
+  return postDoc;
+};
 
 export { getPostService, getAllPostsService, createPostService };
