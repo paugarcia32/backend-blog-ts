18c164e0614361c774b66583e65f4e4febe9496c
Pau Garcia <37461446+paugarcia32@users.noreply.github.com>
2023-08-22 00:45:48 +0200

    Update cors.handle.ts

--- src/utils/cors.handle.ts
+++ src/utils/cors.handle.ts
@@ -1,26 +1,26 @@
 import cors from "cors";
 
-// const whiteList = [process.env.URL, process.env.ADMIN_URL];
+const whiteList = [process.env.URL, process.env.ADMIN_URL];
 // // const whiteList = ["*"];
 
 
-// const corsMiddleware = {
-//   credentials: true,
-//   origin: (
-//     origin: string | undefined,
-//     callback: (error: Error | null, allow?: boolean) => void
-//   ) => {
-//     if (!origin || whiteList.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
-
 const corsMiddleware = {
   credentials: true,
-  origin: "*",
+  origin: (
+    origin: string | undefined,
+    callback: (error: Error | null, allow?: boolean) => void
+  ) => {
+    if (!origin || whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
+
+// const corsMiddleware = {
+//   credentials: true,
+//   origin: "*",
+// };
 
 export default cors(corsMiddleware);
